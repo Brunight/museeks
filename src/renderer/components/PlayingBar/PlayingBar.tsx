247a2bb70c4b68f17ffe99b23de023a987b02fed
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2022-12-04 02:17:24 -0300

    fix theme

--- src/renderer/components/PlayingBar/PlayingBar.tsx
+++ src/renderer/components/PlayingBar/PlayingBar.tsx
@@ -5,7 +5,6 @@ import ClickOutHandler from 'react-onclickout';
 
 import Queue from '../Queue/Queue';
 import PlayingBarInfos from '../PlayingBarInfo/PlayingBarInfo';
-import Cover from '../Cover/Cover';
 import { TrackModel, Repeat } from '../../../shared/types/museeks';
 
 import styles from './PlayingBar.module.css';
